48d9b68557369628c01874fe72450daa096a52a2
Lisa Sy <lisasyis@gmail.com>
2018-12-27 20:16:02 -0800

    Add mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
-  <header class="site_header">
-    <div class="site_header_container">
-      <h1 class="site_header_logo header_title_h3">
-        <Link to="/" >
-          {siteTitle}
-        </Link>
-      </h1>
-      <nav class="site_header_nav">
-        <ul>
-          <li>
-            <Link to={`/work/`}>
-              Work
+class Header extends Component {
+
+  state = { showMobileMenu: false }
+  toggleMobileMenu = () => {
+    this.setState({
+      showMobileMenu: !this.state.showMobileMenu
+    })
+  }
+
+  render() {
+    const mobileMenuActive = this.state.showMobileMenu ? 'is-active' : '';
+
+    return (
+      <header class="site_header">
+        <div className="site_menu__mobile">
+          <h1 class="site_menu__logo">
+            <Link to="/" >
+              Lisa Sy
             </Link>
-          </li>
-          <li>
-            <a href="https://www.etsy.com/shop/LisaSyIllustration" target="_blank" rel="noopener noreferrer">
-              Shop
-            </a>
-          </li>
-          <li>
-            <Link to={`/writing/`}>
-              Writing
+          </h1>
+          <a href="" class="nav_toggle" role="button" aria-expanded="false"
+            aria-controls="menu" onClick={this.toggleMobileMenu}>
+            <svg class="menuicon" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
+              <title>Toggle Menu</title>
+              <g>
+                <line class="menuicon__bar" x1="13" y1="16.5" x2="37" y2="16.5"/>
+                <line class="menuicon__bar" x1="13" y1="24.5" x2="37" y2="24.5"/>
+                <line class="menuicon__bar" x1="13" y1="24.5" x2="37" y2="24.5"/>
+                <line class="menuicon__bar" x1="13" y1="32.5" x2="37" y2="32.5"/>
+                <circle class="menuicon__circle" r="23" cx="25" cy="25" />
+              </g>
+            </svg>
+          </a>
+        </div>
+
+        <nav className={mobileMenuActive + " site_menu"} id="nav">
+          <h1 class="site_menu__logo header_title_h3">
+            <Link to="/" >
+              Lisa Sy
             </Link>
-          </li>
-          <li>
-            <Link to="/about/">
-              About
-            </Link>
-          </li>
-          <li>
-            <a href="mailto:hello@lisasy.com">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-)
+          </h1>
+          <div class="site_menu__pages">
+            <ul>
+              <li className="page__index">
+                <Link to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to={`/work/`}>
+                  Work
+                </Link>
+              </li>
+              <li>
+                <a href="https://www.etsy.com/shop/LisaSyIllustration" target="_blank" rel="noopener noreferrer">
+                  Shop
+                </a>
+              </li>
+              <li>
+                <Link to={`/writing/`}>
+                  Writing
+                </Link>
+              </li>
+              <li>
+                <Link to="/about/">
+                  About
+                </Link>
+              </li>
+              <li>
+                <a href="mailto:hello@lisasy.com">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default Header
