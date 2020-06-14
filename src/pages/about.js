3deeae196cdb5f54fdfc07c76e4484482a31ef97
Lisa Sy <lisasyis@gmail.com>
2020-06-13 23:11:00 -0700

    Update homepage typography, spacing, and content

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,20 +17,17 @@ const About = () => (
 
     <section class="about_inner_container container">
       <section class="row_container col_centered">
-        <h4>
-          About me
-        </h4>
-        <p class="paragraph_larger">
+        <p>
           For almost a decade, I've gotten to work as a designer at places like Dropbox, Facebook, thoughtbot, and now, DEV. I've worked at both large companies, small startups, and everything in-between. What I've loved most has been working with a humble group of curious people who challenge each other to rise up.
         </p>
-        <p class="paragraph_larger">
+        <p>
           Outside of my professional design practice, I draw, paint, and write —
           all of which translates into my illustrations, comics and graphic
           narratives. I'm currently working on an autobiographical graphic
           memoir chronicling how my immigrant family and I co-created our American
           identity across our generational differences.
         </p>
-        <p class="paragraph_larger">
+        <p>
           Born and raised in Los Angeles, I became a first-generation college
           graduate in my family when I graduated from <a
           href="https://www.wesleyan.edu/amst/" target="_blank" rel="noopener
